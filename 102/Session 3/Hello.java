ea8d8cc4ee6b1f4e6e17e369e48876f601d3588d
lococod <56770398+lococod@users.noreply.github.com>
2019-11-06 21:36:50 -0500

    new java

--- 102/Session 3/Hello.java
+++ 102/Session 3/Hello.java
@@ -1,8 +1,40 @@
 class Hello {
 public static void main(String[] args) {
   
+String studentName;
+int age = 34;
+double salary = 78022.96;
+boolean student =true;
+boolean old =false;
 
-System.out.println("Hello World!");
+studentName = "Donald";
+
+int num1;
+int num2;
+int result;
+int results;
+
+num1=1;
+num2=1;
+
+result=num1+num2;
+results=result*num2;
+
+System.out.println("The results are :" + result);
+
+
+
+System.out.println("New Results are:" + results);
+System.out.println(studentName + " " + salary);
+
+for(int i=0;i<=15;i+=3)
+{
+  System.out.println(i);
+  if(i==6)
+  {System.out.println("This is 6");
+}
+else{System.out.println("This is not 6");}
+}
 
 }
 }
