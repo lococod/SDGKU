2257f259c0d998ca83c145b193dc5a0bd071d9a9
lococod <56770398+lococod@users.noreply.github.com>
2019-11-06 22:17:33 -0500

    Update Hello.java

--- 102/Session 3/Hello.java
+++ 102/Session 3/Hello.java
@@ -13,10 +13,23 @@ class Hello {
     /*
      * int num1; int num2; int result; int results;
      */
-    String password = "admin";
+    // String password = "admin";
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a Password: ");
+    System.out.println("Enter a Number: ");
     String p = sc.nextLine();
+    System.out.println("Enter a Number: ");
+    String q = sc.nextLine();
+
+    int qq = Integer.parseInt(q);
+    int pp = Integer.parseInt(p);
+
+    if (pp > qq) {
+      System.out.println("The first number is larger than the second: " + p);
+    } else if (qq > pp) {
+      System.out.println("The Second number is larger: " + q);
+    } else {
+      System.out.println("The numbers are equal.");
+    }
 
     // num1 = 1;
     // num2 = 1;
@@ -24,17 +37,15 @@ class Hello {
     // result = num1 + num2;
     // results = result * num2;
 
-    
-    if (p.equals(password)) {
-      System.out.println("Access Granted");
-    }
-    else{
-      System.out.println("Enter the Correct Password :(");
-    }
+    /*
+     * if (p.equals(password)) { System.out.println("Access Granted"); } else{
+     * System.out.println("Enter the Correct Password :("); }
+     */
+
+    /*
+     * for (int i = 0; i <= 15; i += 3) { System.out.println(i); if
+     * (studentName=="Donald") { System.out.println("This is Donald"); } else {
+     * System.out.println("This is not 6"); }
+     */
   }
-  /*
-   * for (int i = 0; i <= 15; i += 3) { System.out.println(i); if
-   * (studentName=="Donald") { System.out.println("This is Donald"); } else {
-   * System.out.println("This is not 6"); }
-   */
 }
