f19ca65b8165f0bd3dd732ff826c91d1ae5a8c12
lococod <56770398+lococod@users.noreply.github.com>
2019-11-13 21:46:57 -0500

    Game over!

--- 102/Video game/Character.java
+++ 102/Video game/Character.java
@@ -1,79 +1,99 @@
-public class Character{
+public class Character {
 
-private  String name;
-private double height;
-private int age;
-private int energy;
-private int mana;
-private int attackPoints;
-private int defensePoints;
+  private String name;
+  private double height;
+  private int age;
+  private int energy;
+  private int mana;
+  private int attackPoints;
+  private int defensePoints;
+  private int lifePoints;
+  private int oldEnergy;
 
-//constructor
-public Character(String name,int height,int age, int energy,int mana,int attackPoints,int defensePoints){
-this.name=name;
-this.height=height;
-this.age=age;
-this.energy=energy;
-this.mana=mana;
-this.attackPoints=attackPoints;
-this.defensePoints=defensePoints;
-}
-//getters
-public String getName(){
-  return this.name;
-}
-public int getHeight(){
-  return this.height;
-}
-public int getAge(){
-  return this.age;
-}
-public int getEnergy(){
-  return this.energy;
-}
+  // constructor
+  public Character(String name, int height, int age, int energy, int mana, int attackPoints, int defensePoints,
+      int lifePoints) {
+    this.name = name;
+    this.height = height;
+    this.age = age;
+    this.energy = energy;
+    this.mana = mana;
+    this.attackPoints = attackPoints;
+    this.defensePoints = defensePoints;
+    this.lifePoints = lifePoints;
+    this.oldEnergy = energy;
+  }
 
-public int getMana(){
-  return this.mana;
-}
+  // getters
+  public String getLifePoints() {
+    return this.lifePoints;
+  }
 
-public int getAttackPoints(){
-  return this.attackPoints;
-}
+  public String getName() {
+    return this.name;
+  }
 
-public int getDefensePoints(){
-  return this.defensePoints;
-}
-//method
+  public int getHeight() {
+    return this.height;
+  }
 
+  public int getAge() {
+    return this.age;
+  }
 
-public void attack(Character opponent){
+  public int getEnergy() {
+    return this.energy;
+  }
 
+  public int getMana() {
+    return this.mana;
+  }
 
-//int newEnergy = opponent.energy-this.attackPoints;
-int newMana = this.mana-5;
-int damage = (15+(this.attackPoints/(this.attackPoints-opponent.defensePoints)));
-int newEnergy = opponent.energy-damage;
+  public int getAttackPoints() {
+    return this.attackPoints;
+  }
 
-opponent.mana=newMana;
-opponent.energy=newEnergy;
-opponent.display();
+  public int getDefensePoints() {
+    return this.defensePoints;
+  }
+  // method
 
-System.out.println(this.name +" is attacking "+opponent.name+" and hits for "+ damage);
+  public void attack(Character opponent) {
 
+    // int newEnergy = opponent.energy-this.attackPoints;
+    int newMana = this.mana - 5;
+    int damage = (50 + (this.attackPoints / (this.attackPoints - opponent.defensePoints)));
+    int newEnergy = opponent.energy - damage;
 
+    opponent.mana = newMana;
+    opponent.energy = newEnergy;
+    opponent.display();
 
+    System.out.println(this.name + " is attacking " + opponent.name + " and hits for " + damage);
 
-}
-public void display(){
-  System.out.println("-----------------------------------------");
-  System.out.println("Name: "+this.name);
-  System.out.println("Energy:  "+this.energy);
-  System.out.println("Mana: "+this.mana);
-  System.out.println("Age: "+this.age);
-  System.out.println("Attack: "+this.attackPoints);
-  System.out.println("Defense: "+this.defensePoints);
+    if (opponent.energy <= 0) {
+      opponent.lifePoints = opponent.lifePoints - 1;
+      opponent.energy = opponent.oldEnergy;
+      opponent.display();
+      }
 
+    if (opponent.lifePoints <= 0){
+      System.out.println("Game Over!");
+      System.exit(1);
+    }
 
-}
-  
+  }
+
+  public void display() {
+    System.out.println("-----------------------------------------");
+    System.out.println("Name: " + this.name);
+    System.out.println("Energy:  " + this.energy);
+    System.out.println("Mana: " + this.mana);
+    System.out.println("Age: " + this.age);
+    System.out.println("Attack: " + this.attackPoints);
+    System.out.println("Defense: " + this.defensePoints);
+    System.out.println("LifePoints: " + this.lifePoints);
+
+  }
+
 }
--- 102/Video game/Game.java
+++ 102/Video game/Game.java
@@ -1,24 +1,70 @@
-public class Game{
+public class Game {
 
-public static void main(String[] args) {
-  Character toad = new Character("toad", 67, 30,300, 180, 20, 87);
-  Character mario = new Character("mario", 75, 30,300, 180, 25, 37);
-  Character luigi = new Character("luigi", 72, 30,300, 180, 40, 67);
+  public static void main(String[] args) {
+    Character toad = new Character("toad", 67, 30, 300, 180, 20, 87, 6);
+    Character mario = new Character("mario", 75, 30, 300, 180, 25, 37, 6);
+    Character luigi = new Character("luigi", 72, 30, 300, 180, 40, 67, 6);
 
- /* System.out.println(toad.getName()+"\n"+toad.getHealth()+"\n"+toad.getMana()+"\n"+toad.getAttackPoints()+"\n"+toad.getDefensePoints());*/
+    /*
+     * System.out.println(toad.getName()+"\n"+toad.getHealth()+"\n"+toad.getMana()+
+     * "\n"+toad.getAttackPoints()+"\n"+toad.getDefensePoints());
+     */
 
- /*toad.display();
- mario.display();
- luigi.display();*/
+    /*
+     * toad.display(); mario.display(); luigi.display();
+     */
 
- toad.attack(mario);
- toad.attack(mario);
- mario.attack(luigi);
- luigi.attack(mario); 
- toad.attack(luigi);
- toad.attack(mario);
- mario.attack(toad);
- luigi.attack(mario);
-}
-  
+    toad.attack(mario);
+    toad.attack(mario);
+    mario.attack(luigi);
+    luigi.attack(mario);
+    toad.attack(luigi);
+    toad.attack(mario);
+    mario.attack(toad);
+    luigi.attack(mario);
+    toad.attack(mario);
+    toad.attack(mario);
+    mario.attack(luigi);
+    luigi.attack(mario);
+    toad.attack(luigi);
+    toad.attack(mario);
+    mario.attack(toad);
+    luigi.attack(mario);
+    toad.attack(mario);
+    toad.attack(mario);
+    mario.attack(luigi);
+    luigi.attack(mario);
+    toad.attack(luigi);
+    toad.attack(mario);
+    mario.attack(toad);
+    luigi.attack(mario);
+    toad.attack(mario);
+    toad.attack(mario);
+    mario.attack(luigi);
+    luigi.attack(mario);
+    toad.attack(luigi);
+    toad.attack(mario);
+    mario.attack(toad);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    mario.attack(luigi);
+    luigi.attack(mario);
+    toad.attack(luigi);
+    toad.attack(mario);
+    mario.attack(toad);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+    luigi.attack(mario);
+
+  }
+
 }
