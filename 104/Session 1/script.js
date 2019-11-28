597f7692d6d2f4b3911649b93a4e80eff04a6f8e
lococod <56770398+lococod@users.noreply.github.com>
2019-11-27 18:59:06 -0800

    intro to javascript

---
+++ 104/Session 1/script.js
@@ -0,0 +1,93 @@
+// alert("Hello World");
+// console.log("Hello from the text.");
+// prompt("Name: ");
+
+// var x;
+// x="Donald";
+// let y;
+// y="Don";
+
+
+
+// console.log(x);
+// x=33;
+// console.log(x);
+// console.log(y);
+// console.time('Hello');
+// console.timeEnd('Hello');
+
+// // console.warn("Warning");
+
+// var num1 = prompt("enter a number");
+// var num2 = prompt("enter a second number");
+// // // console.log(num1+num2);
+// // console.log(num1*num2);
+// // console.log(num1-num2);
+// // console.log(num1/num2);
+
+// // var sentence ="Cohort 6 is the best cohort ever ....";
+// // console.log(sentence);
+
+// // sentence=sentence.concat(" in the creation of the world");
+// // console.log(sentence);
+// // sentence=sentence.replace('best','worst');
+// // console.log(sentence);
+
+// var name = prompt("Please write your name");
+// console.log(name);
+// //document.getElementById('division').innerHTML="Welcome " + name;
+// document.getElementById('division').innerHTML=`Welcome  ${name} `;
+
+// var age = prompt("enter your age");
+// document.getElementById('age').innerHTML=`You are ${age} years old `;
+// document.getElementById('num1').innerHTML=`Num1 ${num1} `;
+// document.getElementById('num2').innerHTML=`Num2 ${num2} `;
+// document.getElementById('sum').innerHTML="Addition is : " + (parseInt(num1)+parseInt(num2)) ;
+// document.getElementById('divide').innerHTML="Division is :" +(parseInt(num1)/parseInt(num2)) ;
+// document.getElementById('subtract').innerHTML="Subtraction is: "+(parseInt(num1)-parseInt(num2)) ;
+// document.getElementById('multiply').innerHTML="Multiplication results are: "+(parseInt(num1)*parseInt(num2)) ;
+
+// console.log(age);
+
+// var email=prompt("Please enter your email: ");
+
+// for(var i=0;i<10;i++){
+// console.log("Hello JS");
+// }
+
+// var n=email.includes("@");
+
+// if(n=== false){
+
+//     console.error('Please enter a real email!')
+// }
+// else{
+//     console.log('Thank you!')
+// }
+
+// var x='5';
+// if(x===5){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+
+// if (num1>num2){
+//     console.log("num1: "+num1+" is greater than num2: "+num2);
+//     }
+//     if(num1==num2){
+//         console.log("They are equal");
+//     }
+//     if(num2>num1){
+//         console.log("num2: "+num2+" is greater than num1: "+num1);
+//     }
+
+    let student = ['donald','don','loco','a','b','c'];
+    
+
+    for(var j=0;j<student.length;j++)
+    {
+        console.log(student[j]);
+        alert(student[j]);
+    }
