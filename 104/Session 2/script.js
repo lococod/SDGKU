eafee34276a4bbcfbdbf83dc2fa60eb36b26db43
lococod <56770398+lococod@users.noreply.github.com>
2019-11-30 10:29:37 -0800

    find largest number

--- 104/Session 2/script.js
+++ 104/Session 2/script.js
@@ -29,51 +29,112 @@
 //     return a+b;
 // }
 
-// console.log(sum2(2,5));
+// // console.log(sum2(2,5));
 
- var a=parseInt(5);
-var b=parseInt(6);
-var c=parseInt(6);
-var d=parseInt(7);
-var e=parseInt(1);
+// var a = parseInt(5);
+// var b = parseInt(6);
+// var c = parseInt(6);
+// var d = parseInt(7);
+// var e = parseInt(1);
 
-const sum3 = (a,b) => a+b;
-let result= sum3(a,b);
-console.log(result);
+// const sum3 = (a, b) => a + b;
+// let result = sum3(a, b);
+// console.log(result);
 
 
-const multiplication = (a,b) =>a*b;
-let result2 = multiplication(a,b);
-console.log(result2);
+// const multiplication = (a, b) => a * b;
+// let result2 = multiplication(a, b);
+// console.log(result2);
 
-const division = (a,b) =>a/b;
-let result3 = division(a,b);
-console.log(result3);
+// const division = (a, b) => a / b;
+// let result3 = division(a, b);
+// console.log(result3);
 
-const subtract = (a,b) =>a-b;
-let result4 = subtract(a,b);
-console.log(result4);
+// const subtract = (a, b) => a - b;
+// let result4 = subtract(a, b);
+// console.log(result4);
 
-const today = new Date();
-let value = today;
+// const today = new Date();
+// let value = today;
 
-let months =['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-console.table(months);
-value = today.getMonth();
-console.log(value);
+// let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+// console.table(months);
+// value = today.getMonth();
+// console.log(value);
 
-const whatDay = new Date;
-let daytoday = whatDay;
+// const whatDay = new Date;
+// let daytoday = whatDay;
 
-let days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-console.table(days);
-daytoday = whatDay.getDay();
-console.log(daytoday);
+// let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// console.table(days);
+// daytoday = whatDay.getDay();
+// console.log(daytoday);
 
-const logged = false;
-if(logged){
-    console.log("Welcome to the system");
+// const logged = true;
+// if (logged) {
+//     console.log("Welcome to the system");
+// } else {
+//     console.log("You have been denied");
+// }
+
+// console.log(logged ? 'Welcome to the system' : 'You have been denied')
+
+// let money = 500;
+// let cart = 1700;
+// let debitCard = 1000;
+// if (money > cart) {
+//     console.log("Payment Successful");
+// } else if (money + debitCard > cart) {
+//     console.log("You have paid with your Debit Card");
+// } else
+//     console.log("You do not have enough funds");
+
+
+// //&& a= and
+
+// let hour = 15;
+// if (hour > 0 && hour <= 12) {
+//     console.log("French");
+// } else if (hour > 12 && hour <= 19) {
+//     console.log("Spain");
+// }
+
+// var num1 = prompt("Enter in your Payment Method ");
+// const method = num1;
+// switch (method) {
+//     case 'check':
+//         console.log("check selected");
+//         break;
+//     case 'credit':
+//         console.log("credit selected");
+//         break;
+//     case 'debit':
+//         console.log("debit selected");
+//         break;
+//         default :
+//         console.log("Default selected");
+//         break;
+// }
+
+
+// for (let i=0;i<10; i++){
+// console.log(`Number ${i}`);
+// }
+
+const cart = ['product 1','product 2','product 3','product 4'];
+
+for(let i=0;i<=4;i++){
+    console.log(`The product in the cart is: ${cart}`);
 }
-else{
-    console.log("You have been denied");
+
+let numbers = [3,3,6,22,8,9,17]
+let currentGreatest = 0;
+for(let i=0;i<7;i++){
+if(numbers[i]>currentGreatest)
+{currentGreatest=numbers[i];}
+console.log(currentGreatest);
+break;
 }
+
+console.log(Math.max.apply(null,numbers));
+console.log(numbers.sort(function(a,b){return b-a}));
