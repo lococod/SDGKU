ebe3151a52559379179a205c76e67be48f77a03a
lococod <56770398+lococod@users.noreply.github.com>
2019-12-02 21:24:32 -0500

    new objects

--- 104/Session 3/script2.js
+++ 104/Session 3/script2.js
@@ -99,3 +99,24 @@ console.log(client.data.location.city);
 
 let {country} = client.data.location;
 console.log(country);
+
+class Student {
+    constructor(name, points, grade, age){
+        this.name=name;
+        this.points=points;
+        this.grade=grade;
+        this.age=age;
+    }
+}
+
+const student1 = new Student ('Donald',50,'A',12);
+const student2 = new Student ('Jess',49,'A',12);
+const student3 = new Student ('Bess',48,'A',12);
+const student4 = new Student ('Les',47,'A',12);
+const student5 = new Student ('Wes',46,'A',12);
+const student6 = new Student ('Hess',45,'A',12);
+const student7 = new Student ('Lock',44,'B',12);
+const student8 = new Student ('Ness',43,'B',13);
+
+var students = [student1,student2,student3,student4,student5,student6,student7,student8];
+console.log(students);
