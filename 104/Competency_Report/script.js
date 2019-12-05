005f6f547e7e1f05a799a821ca142a573da951d5
lococod <56770398+lococod@users.noreply.github.com>
2019-12-04 20:01:51 -0800

    attack var player

--- 104/Competency_Report/script.js
+++ 104/Competency_Report/script.js
@@ -1,9 +1,11 @@
+var player1 = "Mario"
+var player2 = "Yoshi"
 var attack = function(){
 
-document.getElementById("console").innerHTML="<p>Attacked Yoshi</p>";
+document.getElementById("console").innerHTML="<p>Attacked </p>"+ player2;
 }
 
 var attack2 = function(){
 
-    document.getElementById("console").innerHTML="<p>Attacked Mario</p>";
+    document.getElementById("console").innerHTML="<p>Attacked </p>" +player1;
     }
