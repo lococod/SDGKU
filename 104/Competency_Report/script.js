72ac13c8ca7f1a4c81d82e7c69e46b97f5050d4d
lococod <56770398+lococod@users.noreply.github.com>
2019-12-07 09:01:54 -0800

    attack button

--- 104/Competency_Report/script.js
+++ 104/Competency_Report/script.js
@@ -1,11 +1,21 @@
 var player1 = "Mario"
 var player2 = "Yoshi"
+var d1 = Math.floor(Math.random() * 6) + 1;
+var d2 = Math.floor(Math.random() * 6) + 1;
+var diceTotal = d1 + d2;
+
+var damage= function(){
+
+    document.getElementById("console").innerHTML = "You rolled "+diceTotal+".";
+}
+
 var attack = function(){
 
-document.getElementById("console").innerHTML="<p>Attacked </p>"+ player2;
+document.getElementById("console").innerHTML="<p>Attacked </p>"+ player2 +"<p> and hits for " +diceTotal+".";
 }
 
 var attack2 = function(){
 
     document.getElementById("console").innerHTML="<p>Attacked </p>" +player1;
     }
+
