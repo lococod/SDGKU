e0012aa28f977c0475e891cd87cade495c37a735
lococod <56770398+lococod@users.noreply.github.com>
2019-12-16 18:21:32 -0800

    register button

--- 105/Session 4/myscript.js
+++ 105/Session 4/myscript.js
@@ -20,8 +20,6 @@ let { name,pets, phone, address: { street, number }, workingHours: { days, open,
 
 document.getElementById("info").innerHTML = `<h2> ${name}</h2>  <p> Address Information: ${number} ${street} </p> <p> Phone: (123) ${phone} </P><p> It is open ${days} from ${open} to ${closing}.</p>`;
 
-document.getElementById("info2").innerHTML = `<h2> ${name}</h2>  <p> Address Information: ${number} ${street} </p> <p> Phone: (123) ${phone} </P><p> It is open ${days} from ${open} to ${closing}.</p>`;
-
 class Pet {
     constructor(name, age, breed, gender, service, ownerName, phoneContact) {
     this.name = name;
@@ -48,70 +46,25 @@ class Pet {
   
 }
 
-const pet1 = new Pet("Maggie",3,"Corgi","Female","Wash","Donald","8675309");
-const pet2 = new Pet("Lulu",12,"Chihuahua","Female","Nail Cut","Donald","8675309");
-const pet3 = new Pet("Domino",8,"Akita","Female","De Shed","Donald","8675309");
 
-console.log(pet1);
-// console.log(pet1.status());
-// console.log(pet1.feed());
-// console.log(pet1.status());
+// salon.pets.push(pet1);
+// salon.pets.push(pet2);
+// salon.pets.push(pet3);
+// console.log(salon.pets);
 
-console.log(pet2);
-// console.log(pet2.ownerInfo());
-// console.log(pet2.status());
-// console.log(pet2.feed());
-// console.log(pet2.status());
 
-console.log(pet3);
-// console.log(pet3.ownerInfo());
-// console.log(pet3.status());
-// console.log(pet3.feed());
-// console.log(pet3.status());
 
-salon.pets.push(pet1);
-salon.pets.push(pet2);
-salon.pets.push(pet3);
-console.log(salon.pets);
 
-//display in alert message the number of pets
 
-// numberOfPets = function(){
-//     alert("we have " + pets.length + " pets in the store.");     
-// }
+var textName = document.getElementById('petName');
+var textAge = document.getElementById('petAge');
+var textGender = document.getElementById('petGender');
+var textService = document.getElementById('petService');
+var textOwnerName = document.getElementById('petOwner');
+var textOwnerPhone = document.getElementById('petPhone');
+var textBreed = document.getElementById('petBreed');
 
-// numberOfPets();
-
-var text=" ";
-for (var i=0;i<salon.pets.length;i++){
-    //console.log(salon.pets[i])
-text += `Name: ${salon.pets[i].name}, Age: ${salon.pets[i].age}, Owner: ${salon.pets[i].ownerName} <br>`;  
-
+function register(){
+    const thePet = new Pet(textName.value,textAge.value,textGender.value,textService.value,textOwnerName.value,textOwnerPhone.value,textBreed.value);
 }
 
-//document.getElementById("pets").innerHTML=text;
-  
-
-// const ex  = document.getElementsByTagName('p');
-// for(var i=0;i<ex.length;i++){
-// ex[i].setAttribute('class','example');
-// }
-
-
-// var heading3= document.createElement('h3');
-// console.log(heading3);
-// //var text = document.createTextNode("my first dom exercise");
-// console.log(text);
-// //heading3.appendChild(text);
-// console.log(heading3);
-
-// //document.getElementById('example').appendChild(heading3);
-
-// // heading3.cloneNode(true);
-// // var col1 = document.getElementById('col1');
-// // col1.insertBefore(heading3);
-
-// document.querySelector('.lead');
-
-// //var deleted = document.querySelector("header h1");
-// //deleted.remove();
