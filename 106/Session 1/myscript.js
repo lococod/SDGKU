68b4d217cc054a09a5f7fd30dd3d3a0b6c43645a
lococod <56770398+lococod@users.noreply.github.com>
2020-01-06 19:44:03 -0800

    css

--- 106/Session 1/myscript.js
+++ 106/Session 1/myscript.js
@@ -1,16 +1,59 @@
-function init (){
-
-console.log("ToDo App working!");
-
-var txt = document.getElementById("txtTest");
-console.log(txt);
-console.log(txt.value);
+function createTodo() {
+    //console.log("btn pressed");
 
 
-sayHello();
+
+    //     var text = document.getElementById("txtText").value;
+    // console.log(text);
+
+
+    //get text
+    var text2 = $("#txtText").val();
+
+    //create Li
+    var li = '<li>' + text2 +' <button id="listButton">Done!</button> </li>';
+
+    // add li to ul
+    $("#pendingList").append(li);
+
+    //console.log(text2);
+//clear input field
+    $("#txtText").val("");
+    //set focus
+    $("#txtText").focus();
+
 }
 
-function sayHello(){
+
+
+function init() {
+
+    //console.log("ToDo App working!");
+    //console.log($);
+
+    //var txt = document.getElementById("txtTest");
+    //console.log(txt);
+    //console.log(txt.value);
+
+
+    //sayHello();
+
+
+
+
+
+    //hook events
+    $("#btnSave").click(createTodo);
+    $("#txtText").keypress(function (e){
+        //console.log(e);
+        if(e.key =="Enter"){
+            createTodo();
+        }
+    })
+
+}
+
+function sayHello() {
     console.log("Donald Lococo");
 }
 
