8f3a18c2c406c6619f2974b016d1b29f9ec2a7f9
lococod <56770398+lococod@users.noreply.github.com>
2020-01-22 19:01:23 -0800

    server messages

--- 107/Session 2/server.js
+++ 107/Session 2/server.js
@@ -15,6 +15,27 @@ app.get('/contact',(req, res) =>{
     res.send("Hello my Contacts!");
 });
 
+app.get('/about',(req, res) =>{
+    console.log("Someone wants the about page");
+    res.send("<h1 style='color:red;'>Donald Lococo</h1>");
+});
+
+app.get('/home',(req, res) =>{
+    console.log("Someone wants the home page");
+    res.send("Hello my Homies!");
+});
+
+app.get('/FAQ',(req, res) =>{
+    console.log("Someone wants the faq page");
+    res.send("Hello my FAQ!");
+});
+
+app.get('/exc/:message',(req, res) =>{
+    console.log("Message from the client: ", req.params.message);
+    res.status(202);
+    res.send("Hello my exc message!");
+});
+
 
 app.listen(8080,function() {
 console.log("Server running at http://localhost:8080");
