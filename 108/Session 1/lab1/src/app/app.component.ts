68875a98b059d4ff15d336891388b7251b7711f4
lococod <56770398+lococod@users.noreply.github.com>
2020-02-01 11:21:47 -0800

    starting angular

---
+++ 108/Session 1/lab1/src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lab1';
+
+  // auto execute function when the component is created
+  constructor() {
+    //variables
+    var name: string = "Donald";
+    console.log(name);
+    var email: string = "lococod@gmail.com";
+    console.log(email);
+    var phone: number = 7022415138;
+    console.log(phone);
+    var age: number = 34;
+    console.log(age);
+    var found: boolean = true;
+    console.log(found);
+    var users: string[] = ["a", "b", "c"];
+    users.push("Lococo");
+    console.log(users);
+
+
+    // declarations
+    var xyz: string;
+
+    // initialization
+
+    xyz = users[0];
+    var price = 99;
+
+    var desc = "Product";
+
+    price = 12;
+    desc = "90";
+    var inStock = false;
+    this.hello();
+
+    //this.time(4);
+  }
+
+  hello() {
+    console.log("this is the hello function");
+    this.time(4);
+  }
+  time(howManyTimes: number) {
+
+    for (var i = 0; i < howManyTimes; i++) {
+      console.log("One Time");
+    }
+
+  }
+
+}
