4f404ad503dc5c6d7584beb58dcc43cb20122efa
lococod <56770398+lococod@users.noreply.github.com>
2020-02-03 19:23:46 -0800

    angular calculator for temp

--- 108/Session 2/ERP2/src/app/components/temp-calculator/temp-calculator.component.ts
+++ 108/Session 2/ERP2/src/app/components/temp-calculator/temp-calculator.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temp-calculator.component.scss']
 })
 export class TempCalculatorComponent implements OnInit {
+  fahrenheit = ""
+  celsius = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+
+  tempConversionF(){
+var res = +this.celsius * (9/5) + 32;
+this.fahrenheit = res.toFixed(2);
+
+
+  }
+
+  tempConversionC(){
+    var res2 = (+this.fahrenheit - 32) * (5/9);
+this.celsius = res2.toFixed(2);
   }
 
 }
