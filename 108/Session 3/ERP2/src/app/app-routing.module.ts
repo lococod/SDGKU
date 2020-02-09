ddd8b4333b38b3b1f5126a6ca8b2161446af81a1
lococod <56770398+lococod@users.noreply.github.com>
2020-02-09 01:07:14 -0800

    nav bar

--- 108/Session 3/ERP2/src/app/app-routing.module.ts
+++ 108/Session 3/ERP2/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
 import { TempCalculatorComponent } from './components/temp-calculator/temp-calculator.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
 // register all routes (paths)
 const routes: Routes = [
-  {path: 'user/register', component: UserRegisterComponent},
-  {path: 'converter', component: TempCalculatorComponent}
-  
+  { path: 'user/register', component: UserRegisterComponent },
+  { path: 'converter', component: TempCalculatorComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user/list', component: UserListComponent }
+
 ];
 
 @NgModule({
