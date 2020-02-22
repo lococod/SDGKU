ae80761efb1c086fbe76ffb8a9c51d64b10ef665
lococod <56770398+lococod@users.noreply.github.com>
2020-02-22 01:47:44 -0800

    login check

--- 108/Session 5/ERP2/src/app/pages/login/login.component.ts
+++ 108/Session 5/ERP2/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService} from './../../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+userName = "";
+password = "";
+registeredUsers = [];
+
+  constructor(private data : DataService) {
+    this.registeredUsers = data.getUsers();
+   }
 
   ngOnInit() {
   }
+
+validateLogin(){
+  for (var i = 0 ; i <this.registeredUsers.length; i++){
+    var user = this.registeredUsers[i];
+    if(user.userName == this.userName && user.password == this.password){
+      console.log ("Login Successful");
+      return true;
+    }
+  }
+  console.log ("Wrong Credentials");
+  return false;
+}
 
 }
