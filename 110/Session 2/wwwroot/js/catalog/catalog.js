77d02aeac4c97a86296531abb63f65f76ec5ffc9
lococod <56770398+lococod@users.noreply.github.com>
2020-03-02 19:31:51 -0800

    car rental

--- 110/Session 2/wwwroot/js/catalog/catalog.js
+++ 110/Session 2/wwwroot/js/catalog/catalog.js
@@ -1,22 +1,40 @@
-
-function fetchCatalog(){
+function fetchCatalog() {
     console.log("Fetching Catalog");
     $.ajax({
         type: 'GET',
         url: '/Catalog/GetCatalog',
-        success: function(res){
+        success: function (res) {
             console.log("from server", res);
+
+            for (var i = 0; i < res.length; i++) {
+                displayCar(res[i]);
+            }
         },
-        error: function(details){
+        error: function (details) {
             console.log("error: ", details);
         }
     })
 }
 
-funtion init(){
+function displayCar(car){
+    var sntx = 
+    `<div class='car'>
+    <img src='${car.ImageURL}'>
+    <label>Item Code:</label>
+    <label class='code'>${car.Description}</label>
+    </div>`;
+   
+    var container = $("#catalog");
+
+    container.append(sntx);
+}
+
+
+
+funtion init() {
     console.log("Catalog Page!");
 
     fetchCatalog();
 }
 
-window.onload = init;
+window.onload = init();
