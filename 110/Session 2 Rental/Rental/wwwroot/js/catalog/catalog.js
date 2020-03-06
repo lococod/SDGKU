8fb8d11a1df069c1870e2262e11adf000c58d7cb
lococod <56770398+lococod@users.noreply.github.com>
2020-03-06 12:00:59 -0800

    edits

--- 110/Session 2 Rental/Rental/wwwroot/js/catalog/catalog.js
+++ 110/Session 2 Rental/Rental/wwwroot/js/catalog/catalog.js
@@ -1,7 +1,25 @@
 
 
+function fetchCatalog(){
+    console.log("Fethcing Catalog!");
+
+    $.ajax({
+        type: 'GET',
+        url: '/Catalog/GetCatalog',
+        success: function (res) {
+            console.log("From server: ", res);
+        },
+        error: function (details) {
+            console.log("Error: ", details);
+        }
+    })
+}
+
+
 function init() {
-console.log("Catalog Page!");
+    console.log("Catalog Page!");
+
+
 
 }
 
