701a9753dc8b848a4ac014a1da32dbd8d18ccbb5
lococod <56770398+lococod@users.noreply.github.com>
2020-03-06 12:39:33 -0800

    show catalog

--- 110/Session 2 Rental/Rental/Controllers/CatalogController.cs
+++ 110/Session 2 Rental/Rental/Controllers/CatalogController.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 using Rental.Models;
 using System.Collections.Generic;
@@ -25,7 +24,7 @@ namespace Rental.Controllers
             c1.Year = 2014;
             c1.Cyls = 8;
             c1.Description = "Hellcat, 'nuff said";
-            c1.DailyPrice = 77400m;
+            c1.DailyPrice = 132m;
             c1.ImageURL = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fst.motortrend.com%2Fuploads%2Fsites%2F5%2F2014%2F12%2F2015-Dodge-Charger-SRT-Hellcat-front-three-quarter-view-in-motion-13.jpg&f=1&nofb=1";
 
             Car c2 = new Car();
@@ -34,7 +33,7 @@ namespace Rental.Controllers
             c2.Year = 2018;
             c2.Cyls = 8;
             c2.Description = "SRT, carry your family in style";
-            c2.DailyPrice = 74299m;
+            c2.DailyPrice = 74m;
             c2.ImageURL = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.PZB6p0CObQBW03oKlejLfAHaE8%26pid%3DApi&f=1";
 
             List<Car> list = new List<Car>();
--- 110/Session 2 Rental/Rental/wwwroot/js/catalog/catalog.js
+++ 110/Session 2 Rental/Rental/wwwroot/js/catalog/catalog.js
@@ -1,27 +1,40 @@
 
 
-function fetchCatalog(){
-    console.log("Fethcing Catalog!");
-
+function fetchCatalog() {
+    console.log("Fetching Catalog");
     $.ajax({
         type: 'GET',
         url: '/Catalog/GetCatalog',
         success: function (res) {
-            console.log("From server: ", res);
+            console.log("from server", res);
+
+            for (var i = 0; i < res.length; i++) {
+                displayCar(res[i]);
+            }
         },
         error: function (details) {
-            console.log("Error: ", details);
+            console.log("error: ", details);
         }
     })
 }
 
+function displayCar(Car){
+    var sntx = 
+`<div class="item">
+<img src='${Car.imageURL}'>
+<label>${Car.year}</label>
+<label>${Car.make}</label>
+<label>${Car.model}</label>
+<label>$${(Car.dailyPrice * 1).toFixed(2)}</label>
+</div>`;
+   
+    var container = $("#catalog");
 
-function init() {
-    console.log("Catalog Page!");
-
-
-
+    container.append(sntx);
 }
 
 
-window.onload = init;
+function init(){
+    fetchCatalog();
+}
+window.onload = init();
