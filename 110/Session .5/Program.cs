26acaa25168fa57ffb7978e4caf2bf1aec90a5ac
lococod <56770398+lococod@users.noreply.github.com>
2020-03-07 08:25:50 -0800

    session 3

--- 110/Session .5/Program.cs
+++ 110/Session .5/Program.cs
@@ -9,33 +9,36 @@ namespace Session_1
             Console.WriteLine("Hello World!");
             Test();
         }
-        static void Test() {
+        static void Test()
+        {
             Console.WriteLine("This is a Method");
             Console.WriteLine("This is method 2 ");
 
             // Variables and Data types
 
-            string firstName = "Donald"            ;
-            Console.WriteLine("My name is "+ firstName);
+            string firstName = "Donald";
+            Console.WriteLine("My name is " + firstName);
 
             int age = 34;
             float price = 12.34f;
             bool isThisCrazy = true;
 
-            Console.WriteLine("My age is "+ age);
+            Console.WriteLine("My age is " + age);
 
             //if
-            if(isThisCrazy)
+            if (isThisCrazy)
             {
                 Console.WriteLine("Yes it is");
             }
-            else{
+            else
+            {
                 Console.WriteLine("No it is not");
             }
 
             //loops
-            for(int i=0; i < 10; i++){
-                Console.WriteLine("Iteration: "+ i);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Iteration: " + i);
             }
         }
     }
