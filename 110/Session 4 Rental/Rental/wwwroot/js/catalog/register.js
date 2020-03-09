4562bbb395a700a1a6f4fd83f754d8b3089bbfe7
lococod <56770398+lococod@users.noreply.github.com>
2020-03-09 00:13:55 -0700

    sqlite

---
+++ 110/Session 4 Rental/Rental/wwwroot/js/catalog/register.js
@@ -0,0 +1,56 @@
+function Car(year,make,model,cyls,dailyPrice,imageURL,passengers,type,description,fuelType)
+{
+    this.year = year;
+    this.make = make;
+    this.model = model;
+    this.cyls = cyls;
+    this.dailyPrice = dailyPrice;
+    this.imageURL = imageURL;
+    this.passengers = passengers;
+    this.type = type;
+    this.description = description;
+    this.fuelType = fuelType;
+
+}
+
+
+
+
+
+function saveCar() {
+    var year = $("#txtYear").val();
+    var make = $("#txtMake").val();
+    var model = $("#txtModel").val();
+    var cyls = $("#txtCyls").val();
+    var dailyPrice = $("#txtDailyPrice").val();
+    var imageURL = $("#txtImageUrl").val();
+    var passengers = $("#txtPassengers").val();
+    var type = $("#txtType").val();
+    var description = $("#txtDescription").val();
+    var fuelType = $("#txtFuel").val();
+
+var theNewCar = new Car(year,make,model,cyls,dailyPrice,imageURL,passengers,type,description,fuelType);
+console.log(theNewCar.make);
+
+$.ajax({
+    url: '/catalog/saveCar',
+    type: 'POST',
+    data: JSON.stringify(theNewCar),
+    contentType: 'application/json',
+    success: function(res){
+        console.log("Server says ", res);
+    },
+    error: function(details){
+        console.log("Error ", details);
+    }
+})
+
+}
+
+function init() {
+    console.log("Register Page!");
+    $("#btnSave").click(saveCar);
+    
+}
+
+window.onload = init;
