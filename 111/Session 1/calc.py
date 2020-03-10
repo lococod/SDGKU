b6fd8e57fb6ffc29617b52fbfe98c2eed953ac77
lococod <56770398+lococod@users.noreply.github.com>
2020-03-09 19:20:33 -0700

    calc

--- 111/Session 1/calc.py
+++ 111/Session 1/calc.py
@@ -1,8 +1,10 @@
-#method at the top
+# method at the top
 def separator():
     print(40 * '-')
+
+
 def menu():
-    print(2 * '\n')
+    print(5 * '\n')
     separator()
     print(' Welcome to PyCalc')
     separator()
@@ -11,42 +13,56 @@ def menu():
     print('[3] - Multiply')
     print('[4] - Divide')
     print('[X] - Exit')
-    
-        
+
+
 # add
 def add(x, y):
-   return x + y
+    return x + y
 
 # subtract
+
+
 def subtract(x, y):
-   return x - y
+    return x - y
 
 # multiple
+
+
 def multiply(x, y):
-   return x * y
+    return x * y
 
 # divide
+
+
 def divide(x, y):
-   return x / y
+    return x / y
 
 
-
+valid_selection = ['1', '2', '3', '4', 'X', 'x']
 selection = ''
 while (selection != 'x'):
     menu()
     selection = input("Enter choice: ")
+    if(selection not in valid_selection):
+        print("Not a valid selection")
+        break
     if(selection != 'x'):
         x = float(input("Enter first number: "))
         y = float(input("Enter second number: "))
-        
-        if(selection  == '1'):
-            print(x,"+",y,"=", add(x,y))
-            
+
+        if selection == '1':
+            print(x, "+", y, "=", add(x, y))
+
         elif selection == '2':
-            print(x,"-",y,"=", subtract(x,y))
+            print(x, "-", y, "=", subtract(x, y))
 
         elif selection == '3':
-            print(x,"*",y,"=", multiply(x,y))
+            print(x, "*", y, "=", multiply(x, y))
 
-        elif selection == '4':
-            print(x,"/",y,"=", divide(x,y))
+        elif selection == '4' and y != 0:
+            print(x, "/", y, "=", divide(x, y))
+
+        elif selection == '4' and y == 0:
+            print("No dividing by 0!")
+
+        input('Press Enter to continue')
