47ed3594650c2c8a329e55f345152dc8d4121777
lococod <56770398+lococod@users.noreply.github.com>
2020-03-21 10:13:54 -0700

    movies class

--- 112/Session 2/vidly/movies/models.py
+++ 112/Session 2/vidly/movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length = 255)
+    star = models.CharField(max_length = 255)
+    release_year = models.IntegerField()
+    Genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    director = models.CharField(max_length = 255)
+    image_url = models.TextField()
+    production_cost = models.IntegerField()
+    filmed_in = models.CharField(max_length = 255)
+    production_company = models.CharField(max_length = 255)
+    fx_company = models.CharField(max_length = 255)
+    sound_company = models.CharField(max_length = 255)
