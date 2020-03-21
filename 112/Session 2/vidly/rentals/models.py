557122ddfc240fc8f7474092caf9762d8a9e6c0d
lococod <56770398+lococod@users.noreply.github.com>
2020-03-21 09:11:32 -0700

    session 2

---
+++ 112/Session 2/vidly/rentals/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length = 255)
+    star = models.CharField(max_length = 255)
+    release_year = models.IntegerField()
+    price = models.FloatField()
+    in_stock = models.IntegerField()
+    Genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    in_4k = models.BooleanField()
+    director = models.CharField(max_length = 255)
+    image_url = models.TextField()
