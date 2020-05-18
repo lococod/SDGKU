27596b9139f43815cd9b4b079ff4c53212c3497a
lococod <56770398+lococod@users.noreply.github.com>
2020-05-18 10:13:23 -0700

    create a note

--- NotesApp/NotesApp/src/app/folder/folder.page.ts
+++ NotesApp/NotesApp/src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { NotesService } from '../services/notes.service';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +11,40 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, public notesService: NotesService, private alertCtrl: AlertController, private navCtrl: NavController) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.notesService.load();
   }
+
+  addNote(){
+
+    this.alertCtrl.create({
+      header: 'New Note',
+      message: 'What should the title of this note be?',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.notesService.createNote(data.title);
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    });
+
+  }
+
 
 }
