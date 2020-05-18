c008d721167beb423d98f2778322aa7443a8ebf1
lococod <56770398+lococod@users.noreply.github.com>
2020-05-18 10:07:00 -0700

    baseline

--- NotesApp/NotesApp/src/app/app-routing.module.ts
+++ NotesApp/NotesApp/src/app/app-routing.module.ts
@@ -10,6 +10,13 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'detail',
+    loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
+  },
+  { 
+    path: "notes/:id", loadChildren: "./detail/detail.module#DetailPageModule" 
   }
 ];
 
