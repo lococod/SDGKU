bcdb360dc4c3a00d54f2cbb4d3d7b682a9a590cb
lococod <56770398+lococod@users.noreply.github.com>
2020-05-23 11:55:49 -0700

    delete members and dashboard

--- NotesApp Login/NotesApp/src/app/app-routing.module.ts
+++ NotesApp Login/NotesApp/src/app/app-routing.module.ts
@@ -38,11 +38,11 @@ const routes: Routes = [
   //   path: 'dashboard',
   //   loadChildren: () => import('./members/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   // }
-  {
-    path: 'members',
-    canActivate: [AuthGuardService],
-    loadChildren: './members/member-routing.module#MemberRoutingModule'
-  }
+  // {
+  //   path: 'members',
+  //   canActivate: [AuthGuardService],
+  //   loadChildren: './members/member-routing.module#MemberRoutingModule'
+  // }
 
 
 ];
